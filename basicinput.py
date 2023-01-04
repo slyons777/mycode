c9eb075dcc70c647833c199ef4291f16a97bf823
slyons777 <sarinalyons@gmail.com>
2023-01-04 16:47:26 +0000

    basic input w/python

---
+++ basicinput.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+def main():
+    # ask for user name
+    user_input = input("Hi, what is your name?")
+
+    # ask for day of week 
+    day = input("What day of the week is it?")
+
+    # print out full statement
+    print("Hello, " + user_input + "!" + " Happy " + day + "!" )
+
+
+# call main
+main()
+
