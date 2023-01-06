6e619454086398e1e20892a5f04f00bc8fdff4d0
slyons777 <sarinalyons@gmail.com>
2023-01-06 20:50:48 +0000

    Playing with python pip packages

---
+++ if_name/first_module.py
@@ -0,0 +1,14 @@
+#/usr/bin/env python 
+"""
+Sarina | sarina.lyons@tlgcohort.com
+"""
+
+print("This code will always execute")
+
+def main():
+    print("This code belongs to the main function in module 1.")
+
+if __name__ == "__main__":
+    main()
+
+
