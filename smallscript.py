b45137fa1eff9ffb0a903b56d41b3f47970422f1
slyons777 <sarinalyons@gmail.com>
2023-01-09 20:33:34 +0000

    Learning about classes

---
+++ smallscript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Sarina Lyons | sarina.lyons@tlgcohort.com
+   Best Practice and pylint"""
+
+def main():
+    """Our main program"""
+    mynote = 'print this string' # define mynote
+    print(mynote)                # display our string
+
+main()
+        
