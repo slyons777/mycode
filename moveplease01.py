d123b0ebac3c8ce07dbe429880bd6d3fee5a35a6
slyons777 <sarinalyons@gmail.com>
2023-01-09 08:14:37 +0000

    renaming and moving files

--- moveplease01.py
+++ moveplease01.py
@@ -1,1 +1,21 @@
-i
+#!/usr/bin/env python3
+
+"""Sarina Lyons | sarina.lyons@tlgcohort.com
+   Moving Files Practice"""
+
+
+# bring in the necessary imports
+import shutil
+import os
+
+# have Python start the program in the home user directory
+os.chdir('/home/student/mycode/')
+
+# move file/folder to desired destination
+shutil.move('raynor.obj', 'ceph_storage/')
+
+# prompt user for renaming
+xname = input('What is the new name for kerrigan.obj? ')
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
+
+
