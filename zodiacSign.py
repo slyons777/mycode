afe938d86c0e678d946a7a2561d0a2564b22f1d7
slyons777 <sarinalyons@gmail.com>
2023-01-09 07:25:16 +0000

    Zodiac Sign Redo

--- zodiacSign.py
+++ zodiacSign.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python3
 
+"""Sarina Lyons | sarina.lyons@tlgcohort.com
+   Write your own if-logic script - Zodiac Sign"""
+
 # This is an app designed to tell a user their zodiac sign, based on their birtdate. # User will be prompted to enter birth day and month and their zodiac sign should be # printed to the screen. Error checking should be done to ensure user is entering 
 # appropriate characters depending upon input prompt i.e. (letters for name, numbers # for date)
 
-def main():
-    # Welcome user and prompt for name
-    user_name = input("Welcome to SunSigns! What's your name? ")
+def determine_zodiac(month, day):
+  # Dictionary mapping zodiac signs to start and end dates
+  zodiac_dates = {
+    "Aries": (3, 21, 4, 19),
+    "Taurus": (4, 20, 5, 20),
+    "Gemini": (5, 21, 6, 20),
+    "Cancer": (6, 21, 7, 22),
+    "Leo": (7, 23, 8, 22),
+    "Virgo": (8, 23, 9, 22),
+    "Libra": (9, 23, 10, 22),
+    "Scorpio": (10, 23, 11, 21),
+    "Sagittarius": (11, 22, 12, 21),
+    "Capricorn": (12, 22, 1, 19),
+    "Aquarius": (1, 20, 2, 18),
+    "Pisces": (2, 19, 3, 20)
+  }
 
-    # prompt for month, day
-    month = input("Thanks! Now how about your birth month?").lower()
-    day = int(input("Now the day? "))
+  # Iterate through the dictionary until we find the correct zodiac sign
+  while True:
+    for sign, dates in zodiac_dates.items():
+      start_month, start_day, end_month, end_day = dates
+      if (month == start_month and day >= start_day) or (month == end_month and day <= end_day):
+        return sign
 
-    # check against month and date input to get sign
-    if month == 'december':
-	    zodiac_sign = 'Sagittarius' if (day < 22) else 'Capricorn'
-    elif month == 'january':
-	    zodiac_sign = 'Capricorn' if (day < 20) else 'Aquarius'
-    elif month == 'february':
-	    zodiac_sign = 'Aquarius' if (day < 19) else 'Pisces'
-    elif month == 'march':
-	    zodiac_sign = 'Pisces' if (day < 21) else 'Aries'
-    elif month == 'april':
-	    zodiac_sign = 'Aries' if (day < 20) else 'Taurus'
-    elif month == 'may':
-	    zodiac_sign = 'Taurus' if (day < 21) else 'Gemini'
-    elif month == 'june':
-	    zodiac_sign = 'Gemini' if (day < 21) else 'Cancer'
-    elif month == 'july':
-	    zodiac_sign = 'Cancer' if (day < 23) else 'Leo'
-    elif month == 'august':
-	    zodiac_sign = 'Leo' if (day < 23) else 'Virgo'
-    elif month == 'september':
-	    zodiac_sign = 'Virgo' if (day < 23) else 'Libra'
-    elif month == 'october':
-	    zodiac_sign = 'Libra' if (day < 23) else 'Scorpio'
-    elif month == 'november':
-	    zodiac_sign = 'scorpio' if (day < 22) else 'Sagittarius'
+# Prompt the user to enter their birthday
+birthday = input("Please enter your birthday (MM/DD): ")
 
-    print("Awesome looks like you're a " + zodiac_sign + "!")
+# Split the birthday into the month and day
+month, day = map(int, birthday.split("/"))
 
-if __name__ == "__main__":
-    main()
+# Determine the user's zodiac sign
+sign = determine_zodiac(month, day)
+
+# Print the user's zodiac sign
+print(f"Awesome your zodiac sign is: {sign}")
+
+
+if __name__ == "__determine_zodiac__":
+    determine_zodiac()
