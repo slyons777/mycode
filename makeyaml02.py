82be306502ef953cd7dd6d70f51e2209c509a0ff
slyons777 <sarinalyons@gmail.com>
2023-01-10 23:03:38 +0000

    learning about YAML

---
+++ makeyaml02.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Sarina Lyons | sarina.lyons@tlgcohort.com
+   yaml.dumps() expects a single argument
+   performs the YAML transformation,
+   and returns that as a YAML string"""
+
+# YAML is NOT part of the standard library
+# python3 -m pip install pyyaml
+import yaml
+
+def main():
+    """runtime code"""
+    ## create a blob of data to work with
+    hitchhikers = [{"name": "Zaphod Beeblebrox", "species": "Betelgeusian"},
+      {"name": "Arthur Dent", "species": "Human"}]
+
+    ## display our Python data (a list containing two dictionaries)
+    print(hitchhikers)
+
+    ## Create the YAML string
+    yamlstring = yaml.dump(hitchhikers)
+
+    ## Display a single string of YAML
+    print(yamlstring)
+
+if __name__ == "__main__":
+    main()
+
